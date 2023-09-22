7e0b82594118276601c5e92ea823b8620eb984b6
ManojDhundhalva <manojdhundhalva60@gmail.com>
2023-09-22 23:52:14 +0530

    create-sign-in-page

--- tictak/src/App.js
+++ tictak/src/App.js
@@ -3,21 +3,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* <div className="input-group mb-3">
+        <span className="input-group-text" id="basic-addon1">@</span>
+        <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+        <label for="pwd">Password:</label>
+        <input type="password" id="pwd" name="pwd" /> */}
+      {/* </div> */}
+      <div id="sign-in-container">
+        <form action="/users/create-session" method="post">
+          <label for="email_username">Username or E-Mail</label> <br />
+          <input type="text" id="email_username" name="email_username" placeholder="username/email@abc.com" />
+
+          <br />
+
+          <label for="password">Password
+          </label> <br />
+          <input type="password" id="password" name="password" />
+
+          <br />
+
+          <button type="submit" id="submit">LOGIN</button>
+        </form>
+
+        <div id="newUser">
+          <p>New User?</p>
+          <div>
+            <a href="/users/sign-up">Register</a>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
